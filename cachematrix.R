4a29f7642edd6881f1511147c8baf7e875d686e3
bfilreis1 <43426238+bfilreis1@users.noreply.github.com>
2018-10-05 18:19:04 -0400

    Update cachematrix.R
    first of two functions for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This builds a special matrix to
+##   1. set its value 
+##   2. get its value 
+##   3. set the inverse 
+##   4. get its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
-}
-
+  inv= NULL
+  set = function(a){
+  
+## sets value of matrix 
+    x <<- a
+    inv <<- NULL
+    
+  }
+  get <-function() x
+  setinvfun <- function(inverse) a <<- inv
+## A as inverse of function of X
+  getinvfun <- function() a
+  list(set = set, get = get, setinvfun = setinvfun, getinvfun = getinvfun)
+              }
 
 ## Write a short comment describing this function
 
